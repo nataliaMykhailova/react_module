a813e33d76a5efc84fd7b758c46fb77d0b5e0988
nataliaMykhailova <natalia.mykhailova.95@gmail.com>
2024-05-05 19:54:45 +0300

    commit_lesson2_1

--- src/App.tsx
+++ src/App.tsx
@@ -6,30 +6,30 @@ import {ICommentModel} from "./models/ICommentModel";
 import CommentsComponent from "./components/comments/CommentsComponent";
 import './App.css';
 
-const App:FC = () =>  {
+const App: FC = () => {
     const [selectedUserId, setSelectedUserId] = useState<number>(0);
     const [comments, setComments] = useState<ICommentModel[]>([]);
 
     const liftPosts = (userId: number) => {
-        requests.posts.getPostsOfOneUser(userId).then(({ data }) => {
+        requests.posts.getPostsOfOneUser(userId).then(({data}) => {
             setSelectedUserId(userId);
             setComments([]); // Clear comments when selecting a new user
         });
     };
 
     const liftComments = (postId: number) => {
-        requests.comments.getCommentsOfOnePost(postId).then(({ data }) => {
+        requests.comments.getCommentsOfOnePost(postId).then(({data}) => {
             setComments(data);
         });
     };
-  return (
-      <>
-          <UsersComponent liftPosts={liftPosts} />
-                  <PostsComponent userId={selectedUserId} liftComments={liftComments} />
-                  <CommentsComponent comments={comments} />
+    return (
+        <>
+            <UsersComponent liftPosts={liftPosts}/>
+            <PostsComponent userId={selectedUserId} liftComments={liftComments}/>
+            <CommentsComponent comments={comments}/>
 
-      </>
-  );
+        </>
+    );
 }
 
 export default App;
