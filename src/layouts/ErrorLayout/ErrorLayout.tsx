9f18a0368f8e79f9b03bccf429670ec54b9db1a9
nataliaMykhailova <natalia.mykhailova.95@gmail.com>
2024-05-30 10:47:56 +0300

    commit_lesson7_2

--- src/layouts/ErrorLayout/ErrorLayout.tsx
+++ src/layouts/ErrorLayout/ErrorLayout.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import HeaderComponent from "../../components/HeaderComponent/HeaderComponent";
+import FooterComponent from "../../components/FooterComponent/FooterComponent";
 
 const ErrorLayout = () => {
     return (
         <div>
+            <HeaderComponent/>
+            <h1>Display error, please, check your path!!!</h1>
+            <FooterComponent/>
 
         </div>
     );
